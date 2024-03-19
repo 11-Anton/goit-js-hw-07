dd353f3acc05ed8c59e62511a954ad35898aecff
Anton Mametov <joyfulanton1107@gmail.com>
2024-03-19 19:51:10 +0200

    fin

--- js/task-3.js
+++ js/task-3.js
@@ -6,8 +6,7 @@ input.addEventListener('input', onInputChangeName);
 function onInputChangeName(event) {
     const nameUser = event.currentTarget.value.trim();
     if (nameUser !== '') {
-        
-    
+        span.textContent = nameUser;
     } else {
         span.textContent = 'Anonymous';
     }
